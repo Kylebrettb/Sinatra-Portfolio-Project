4cc81518325109a4b3b8bbcafa95db7d8b742cf7
Kylebrettb <43708844+Kylebrettb@users.noreply.github.com>
2019-11-07 14:20:29 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,1 +1,31 @@
+require './config/environment'
 
+class ApplicationController < Sinatra::Base
+  configure do 
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'secret_family_library'
+  end
+
+  get '/' do 
+    erb :'/home'
+  end
+
+  helpers do
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/login?error=You must be logged in to access your library"
+      end
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+  end
+end
