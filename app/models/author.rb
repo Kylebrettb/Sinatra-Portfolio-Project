7f56d8b81635dfa9f82bd7dfbf88658705c239c3
Kylebrettb <43708844+Kylebrettb@users.noreply.github.com>
2019-11-07 14:23:26 -0700

    Update author.rb

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,1 +1,10 @@
+class Author < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  has_many :books
 
+  def display_name
+    # Rowling, J.K
+    self.last_name + ", " + self.first_name
+  end
+end
